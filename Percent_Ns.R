6ff366f7d3e18bbdbc4fb204775db4b4413ffac9
RowanDurrant <58739837+RowanDurrant@users.noreply.github.com>
2023-03-09 12:44:21 +0000

    add df output capability

--- Percent_Ns.R
+++ Percent_Ns.R
@@ -1,14 +1,28 @@
 library(Biostrings)
 
-Percent_Ns = function(filename){
+Percent_Ns = function(filename, output.as.df = F){
   x = readDNAStringSet(filename)
-
-  for(i in 1:length(x)){
+  
+  if(output.as.df == F){
+      for(i in 1:length(x)){
     freqs = alphabetFrequency(x[i], baseOnly = F, as.prob = F)
     N_percent = (sum(freqs[colnames(freqs) %in% c("n", "N")])/sum(freqs)) * 100
       message("\n", names(x[i]), " contains ", N_percent,"% Ns.")
 
   }
   message("\nEnd of warnings.")
+    
+  }
+  if(output.as.df == T){
+    ID = names(x)
+    N_percent = rep(NA, length(x))
+    for(i in 1:length(x)){
+      freqs = alphabetFrequency(x[i], baseOnly = F, as.prob = F)
+      N_percent[i] = (sum(freqs[colnames(freqs) %in% c("n", "N")])/sum(freqs)) * 100
+      
+    }
+    df = as.data.frame(cbind(ID,N_percent))
+    return(df)
+  }
   
 }
